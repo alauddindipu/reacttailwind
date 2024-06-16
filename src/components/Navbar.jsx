1325ed363351b922ca621d5980b0689b61aa5484
alauddinDipu <alauddin.cucse@gmail.com>
2024-06-17 00:54:14 +0600

    selection color set cyan added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-   <nav className="bg-red-800 mb-20 flex item-center justify-between py-6">
+   <nav className="mb-20 flex item-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
                 <img src={logo} alt="logo" />
             </div>
