58e5438eab865b44974e8a7b087159231dfc1e25
alauddinDipu <alauddin.cucse@gmail.com>
2024-06-17 00:32:32 +0600

    social icon and bbackground created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar"
 const App = () => {
   return (
     <div>
+
+<div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+
          <Navbar />
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import logo from "../assets/kevinRushLogo.png"
-
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className="bg-red-900 mb-20 flex item-center justify-between py-6">
+  return (
+   <nav className="bg-red-800 mb-20 flex item-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
                 <img src={logo} alt="logo" />
             </div>
-        </nav>
-  
-}
+          
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+                <FaLinkedin />
+                <FaGithub />
+                <FaSquareXTwitter />
+                <FaInstagram />
+            </div>
 
-export default Navbar
+    </nav>
+    );
+  
+};
+
+export default Navbar;
